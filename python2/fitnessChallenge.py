3471c9905a35b297027fc6d176a11e50b42bfcd2
inaram <inaram@users.noreply.github.com>
2016-10-22 16:11:48 -0700

    initial version of fitnessChallenge.py

---
+++ python2/fitnessChallenge.py
@@ -0,0 +1,25 @@
+def fitnessChallenge():
+    challengeLength = int(raw_input("How many days do you want your fitness challenge to last? "))
+    totalMinutes = 0
+    totalDays = 0 
+
+    for day in range(1, challengeLength + 1):
+        answer = raw_input("Have you exercised today? (y/n): ")
+        if answer == "y" or answer == "yes" or answer == "Y" or answer == "Yes" or answer == "YES":
+            totalDays += 1
+            print "Great job!"
+            addMinutes = int(raw_input("For how many minutes? "))
+            totalMinutes = totalMinutes + addMinutes
+            print "Keep up the good work!"
+            
+    if totalMinutes != 0 or totalDays != 0:
+        print "Congratulations! You have exercised for %d day(s) out of your %d-day challenge" % (totalDays, challengeLength)
+        print "Total number of minutes:", totalMinutes
+
+def main():
+    fitnessChallenge()
+
+if __name__ == "__main__":
+	main()
+	
+
